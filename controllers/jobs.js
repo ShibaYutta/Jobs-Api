18508aab405a5c52c2c36a96e1ff8f7ad2a9b4f5
ShibaYutta <73189830+ShibaYutta@users.noreply.github.com>
2021-11-25 18:10:59 -0300

    Day 3
    Done all CRUD methods for jobs

--- controllers/jobs.js
+++ controllers/jobs.js
@@ -1,21 +1,73 @@
+const Job = require("../models/Job");
+const { StatusCodes } = require("http-status-codes");
+const { BadRequestError, NotFoundError } = require("../errors");
+
 const getAllJobs = async (req, res) => {
-  res.send("get all the jobs");
+  const jobs = await Job.find({ createdBy: req.user.userId }).sort("createdAt");
+  res.status(StatusCodes.OK).json({ jobs, count: jobs.length });
 };
 
 const getJob = async (req, res) => {
-  res.send("get job");
+  const {
+    user: { userId },
+    params: { id: jobId },
+  } = req;
+
+  const job = await Job.findOne({
+    _id: jobId,
+    createdBy: userId,
+  });
+  if (!job) {
+    throw new NotFoundError(`No job with id ${jobId}`);
+  }
+  res.status(StatusCodes.OK).json({ job });
 };
 
 const createJob = async (req, res) => {
-  res.json(req.user);
+  if (!req.user.userId) {
+    return res.status(StatusCodes.UNAUTHORIZED).send("Empty id");
+  }
+  req.body.createdBy = req.user.userId;
+  const job = await Job.create(req.body);
+  res.status(StatusCodes.CREATED).json({ job });
 };
 
 const updateJob = async (req, res) => {
-  res.send("update job");
+  const {
+    user: { userId },
+    params: { id: jobId },
+    body: { company, position },
+  } = req;
+
+  if (!company || !position) {
+    throw new BadRequestError("Company or Position fields cannot be empty");
+  }
+  const job = await Job.findByIdAndUpdate(
+    { _id: jobId, createdBy: userId },
+    req.body,
+    { new: true, runValidators: true }
+  );
+  if (!job) {
+    throw new NotFoundError(`No job with id ${jobId}`);
+  }
+  res.status(StatusCodes.OK).json({ job });
 };
 
 const deleteJob = async (req, res) => {
-  res.send("delete job");
+  const {
+    user: { userId },
+    params: { id: jobId },
+  } = req;
+
+  const job = await Job.findByIdAndRemove({
+    _id: jobId,
+    createdBy: userId,
+  });
+
+  if (!job) {
+    throw new NotFoundError(`No job with id ${jobId}`);
+  }
+  res.status(StatusCodes.OK).send();
 };
 
 module.exports = {
--- models/Job.js
+++ models/Job.js
@@ -1,23 +1,29 @@
 const mongoose = require("mongoose");
 
-const JobSchema = new mongoose.Schema({
-  company: {
-    type: String,
-    required: [true, "Please provide company name"],
-    maxlenght: 50,
+const JobSchema = new mongoose.Schema(
+  {
+    company: {
+      type: String,
+      required: [true, "Please provide company name"],
+      maxlenght: 50,
+    },
+    position: {
+      type: String,
+      required: [true, "Please provide position"],
+      maxlenght: 100,
+    },
+    status: {
+      type: String,
+      enum: ["interview", "declined", "pending"],
+      default: "pending",
+    },
+    createdBy: {
+      type: mongoose.Types.ObjectId,
+      ref: "User",
+      require: [true, "Please provide a user"],
+    },
   },
-  position: {
-    type: String,
-    required: [true, "Please provide position"],
-    maxlenght: 100,
-  },
-  status: {
-    type: String,
-    enum: ["interview", "declined", "pending"],
-    default: "pending",
-  },
-  createdBy: {
-    type: mongoose.Types.ObjectId,
-    ref: "User",
-  },
-});
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Job", JobSchema);
