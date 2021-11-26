23a9f0084997a394a37ccacfc282b2221d4890ea
ShibaYutta <73189830+ShibaYutta@users.noreply.github.com>
2021-11-26 18:31:08 -0300

    fim
    fim

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -7,13 +7,25 @@ const errorHandlerMiddleware = (err, req, res, next) => {
     msg: err.message || "something went wrong try again later",
   };
 
-  if (err instanceof CustomAPIError) {
-    return res.status(err.statusCode).json({ msg: err.message });
-  }
-  if (err.code && err.code == 11000) {
-    customError.msg = `Duplicate value entered for ${err} field, please choose another value`;
+  // if (err instanceof CustomAPIError) {
+  //   return res.status(err.statusCode).json({ msg: err.message });
+  // }
+  if (err.name === "ValidationError") {
+    customError.msg = Object.values(err.errors)
+      .map((item) => item.message)
+      .join(", ");
     customError.statusCode = StatusCodes.BAD_REQUEST;
   }
+  if (err.name === "CastError") {
+    customError.msg = `no item found with id: ${err.value} `;
+    customError.statusCode = StatusCodes.NOT_FOUND;
+  }
+  if (err.code && err.code === 11000) {
+    // Duplicate Mongose
+    customError.msg = `Duplicate value entered for ${err.keyValue} field, please choose another value`;
+    customError.statusCode = StatusCodes.BAD_REQUEST;
+  }
+  // return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ err });
   return res.status(customError.statusCode).json({ msg: customError.msg });
 };
 
